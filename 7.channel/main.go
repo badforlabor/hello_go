b3344519061f65e7c967a19f79715a186919df82
badlabor <badlabor@work.com>
2017-07-14 16:34:38 +0800

    channel例子

--- 7.channel/main.go
+++ 7.channel/main.go
@@ -10,6 +10,11 @@ import (
 */
 
 func main() {
+	//testChannelRange()
+	testChannelRangeSelect()
+}
+
+func testChannelRange() {
 
 	defer func() {
 		fmt.Println("exit.")
@@ -35,6 +40,66 @@ func main() {
 	data <- 2
 	data <- 3
 	close(data)
+
+	fmt.Println("send over.")
+	<-exit
+}
+
+func testChannelRangeSelect() {
+	defer func() {
+		fmt.Println("exit.")
+	}()
+
+	data := make(chan int)
+	exit := make(chan bool)
+	quit := make(chan bool)
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		q := false
+		lastTime := time.Now()
+		for !q {
+
+			empty := false
+			for !empty {
+				select {
+				case d := <-data:
+					fmt.Println("data:", d)
+				default:
+					empty = true
+				}
+			}
+
+			select {
+			case <-quit:
+				q = true
+			default:
+			}
+
+			tNow := time.Now()
+			frame := time.Second / 30
+			if tNow.Sub(lastTime) < frame {
+				fmt.Println("time:", time.Now().String())
+				time.Sleep(frame - tNow.Sub(lastTime))
+			}
+			fmt.Println("time delta:", tNow.Sub(lastTime))
+			lastTime = tNow
+		}
+		fmt.Println("recv over.")
+		exit <- true
+	}()
+	go func() {
+		for false {
+			fmt.Println("echo.")
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	data <- 1
+	data <- 2
+	data <- 3
+	quit <- true // 执行这条语句之后，会让select成功，然后整个进程退出
+	//close(data)
 
 	fmt.Println("send over.")
 	<-exit
