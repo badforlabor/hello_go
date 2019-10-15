5d12b8e16f0c0fb2cb5516cbc2ae0391edf77de1
liubo04 <liubo04@work.com>
2019-10-15 17:08:33 +0800

    更多测试

---
+++ 13.env/main.go
@@ -0,0 +1,35 @@
+/**
+ * Auth :   liubo
+ * Date :   2019/10/15 9:46
+ * Comment: 测试环境变量
+ */
+
+package main
+
+import (
+	"fmt"
+	"golang.org/x/sys/windows/registry"
+)
+
+func main() {
+
+	key, err := registry.OpenKey(registry.LOCAL_MACHINE, "SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment", registry.ALL_ACCESS)
+	if err == nil {
+		v, _, err := key.GetStringValue("Path")
+		if err == nil {
+			fmt.Printf("系统环境变量：%s\n", v)
+		}
+		key.Close()
+	}
+	key, err = registry.OpenKey(registry.CURRENT_USER, "Environment", registry.ALL_ACCESS)
+	if err == nil {
+		v, _, err := key.GetStringValue("Path")
+		if err == nil {
+			fmt.Printf("用户环境变量：%s\n", v)
+		}
+		key.SetStringValue("aaa", "bbb")
+		key.DeleteValue("aaa")
+
+		key.Close()
+	}
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module hell_go
+
+go 1.13
+
+require (
+	github.com/lxn/walk v0.0.0-20191001144247-31870cf268b0
+	github.com/lxn/win v0.0.0-20190919090605-24c5960b03d8 // indirect
+	golang.org/x/sys v0.0.0-20191010194322-b09406accb47
+	gopkg.in/Knetic/govaluate.v3 v3.0.0 // indirect
+)
